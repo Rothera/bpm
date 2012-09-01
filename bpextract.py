dadd7571b3fc91be11872817ced2cc51c4c13e42
Rothera <rothera@users.noreply.github.com>
2012-08-31 20:07:57 -0400

    Factored more emote extraction out

--- bpextract.py
+++ bpextract.py
@@ -21,91 +21,6 @@ import bplib
 import bplib.css
 import bplib.extract
 
-### Spritesheeting
-
-def build_spritesheet_map(emotes):
-    spritesheets = {}
-
-    for (name_pair, raw_emote) in emotes.items():
-        image_url = get_url(bplib.css.get_prop(raw_emote.css.pop("background-image")))
-        if image_url not in spritesheets:
-            spritesheets[image_url] = {}
-        spritesheets[image_url][name_pair] = convert_emote(name_pair, image_url, raw_emote)
-
-    for (image_url, ss_map) in spritesheets.items():
-        verify_spritesheet(image_url, ss_map)
-
-    return spritesheets
-
-def get_url(text):
-    if text.startswith("url(") and text.endswith(")"):
-        return text[4:-1]
-    raise ValueError("Invalid URL")
-
-def convert_emote(name_pair, image_url, raw_emote):
-    css = raw_emote.css.copy()
-    def try_pop(prop):
-        if prop in css:
-            del css[prop]
-
-    try_pop("display")
-    try_pop("clear")
-    try_pop("float")
-
-    width = parse_size(bplib.css.get_prop(css.pop("width")))
-    height = parse_size(bplib.css.get_prop(css.pop("height")))
-    size = (width, height)
-
-    if "background-position" in css:
-        offset = parse_position(bplib.css.get_prop(css["background-position"]), width, height)
-        del css["background-position"]
-    else:
-        offset = None
-
-    # Remove some annoying, commonly seen properties
-    for p in ("background-repeat",):
-        if p in css:
-            del css[p]
-    for p in css:
-        # These properties are also included a lot, so omitted from logs for brevity.
-        if p not in ("margin-left", "margin-right"):
-            print("WARNING: emote %r has extra property %r (%r)" % (name_pair, p, css[p]))
-
-    return bplib.Emote(name_pair[0], name_pair[1], css, size, offset, image_url)
-
-def parse_size(sz):
-    # Plain numbers are typically zero, though there are some unusual emotes
-    # with positive offsets
-    if sz.endswith("px"):
-        sz = sz[:-2]
-    return int(sz)
-
-def parse_position(pos, width, height):
-    (str_x, str_y) = pos.split()
-    return (parse_pos(str_x, width), parse_pos(str_y, height))
-
-def parse_pos(s, size):
-    # Hack to handle percentage values, which are essentially multiples of the
-    # width/height. Used in r/mylittlelistentothis for some crazy reason.
-    if s[-1] == "%":
-        # Non-multiples of 100 won't work too well here, but who would do that
-        # anyway
-        return int(int(s[:-1]) / 100.0 * size)
-    else:
-        return parse_size(s)
-
-def verify_spritesheet(image_url, emotes):
-    # Ensure that all emotes have a bg-position. Only one may lack one.
-    unpositioned_emotes = []
-    for (name_pair, emote) in emotes.items():
-        if emote.offset is None:
-            emote.offset = (0, 0)
-            unpositioned_emotes.append(name_pair)
-
-    if len(unpositioned_emotes) > 1:
-        print("ERROR: Multiple unsized emotes within spritesheet %r: %s" % (
-            image_url, " ".join(map(repr, unpositioned_emotes))))
-
 ### Serialization
 
 def generate_meta(src_filename, name, display_name):
@@ -141,8 +56,8 @@ def main():
     partial_emotes = bplib.extract.extract_raw_emotes(args, css_rules)
     emote_map = bplib.extract.build_emote_map(partial_emotes)
     bplib.extract.collapse_specials_properties(emote_map)
-    normal_emotes, custom_emotes = classify_emotes(emote_map)
-    spritesheets = build_spritesheet_map(normal_emotes)
+    normal_emotes, custom_emotes = bplib.extract.classify_emotes(emote_map)
+    spritesheets = bplib.extract.build_spritesheet_map(normal_emotes)
 
     data = {
         "Meta": generate_meta(args.css.name, args.name, args.displayname),
