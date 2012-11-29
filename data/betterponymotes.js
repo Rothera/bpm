0d85457c10b3fab06ee508cec9387f027d9e0efb
Rothera <rothera@users.noreply.github.com>
2012-11-28 22:19:35 -0500

    Release v49.80 with some shiny new emotes

--- data/betterponymotes.js
+++ data/betterponymotes.js
@@ -8,11 +8,11 @@
 // @include https://*/*
 // @name BetterPonymotes
 // @namespace http://rainbow.mlas1.us/
-// @require bpm-data.js?p=2&dver=79
+// @require bpm-data.js?p=2&dver=80
 // @require pref-setup.js?p=2&cver=49
 // @run-at document-start
 // @updateURL http://rainbow.mlas1.us/betterponymotes.user.js
-// @version 49.79
+// @version 49.80
 // ==/UserScript==
 
 /*******************************************************************************
@@ -36,7 +36,7 @@ var emote_map, sr_name2id, sr_id2name, tag_name2id, tag_id2name, bpm_backendsupp
 "use strict";
 
 var BPM_CODE_VERSION = "48";
-var BPM_DATA_VERSION = "78";
+var BPM_DATA_VERSION = "80";
 var BPM_RESOURCE_PREFIX = "http://rainbow.mlas1.us";
 var BPM_OPTIONS_PAGE = BPM_RESOURCE_PREFIX + "/options.html";
 
