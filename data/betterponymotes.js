1cf5dc50d3a7f0a51dc1087720a40de813466afe
Rothera <rothera@users.noreply.github.com>
2012-12-02 20:56:27 -0500

    Release v49.81.1 to fix a broken spritesheet

--- data/betterponymotes.js
+++ data/betterponymotes.js
@@ -8,11 +8,11 @@
 // @include https://*/*
 // @name BetterPonymotes
 // @namespace http://rainbow.mlas1.us/
-// @require bpm-data.js?p=2&dver=81
+// @require bpm-data.js?p=2&dver=81_1
 // @require pref-setup.js?p=2&cver=49
 // @run-at document-start
 // @updateURL http://rainbow.mlas1.us/betterponymotes.user.js
-// @version 49.81
+// @version 49.81.1
 // ==/UserScript==
 
 /*******************************************************************************
@@ -35,8 +35,8 @@ var emote_map, sr_name2id, sr_id2name, tag_name2id, tag_id2name, bpm_backendsupp
 (function(_bpm_this) {
 "use strict";
 
-var BPM_CODE_VERSION = "48";
-var BPM_DATA_VERSION = "81";
+var BPM_CODE_VERSION = "49";
+var BPM_DATA_VERSION = "81_1";
 var BPM_RESOURCE_PREFIX = "http://rainbow.mlas1.us";
 var BPM_OPTIONS_PAGE = BPM_RESOURCE_PREFIX + "/options.html";
 
