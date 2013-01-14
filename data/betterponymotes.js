6b5105465fc535a112440d3dc21c4143b2e3f1a0
Rothera <rothera@users.noreply.github.com>
2013-01-13 23:36:55 -0500

    Release v56.87 with a week's worth of emotes and a log dump button

--- data/betterponymotes.js
+++ data/betterponymotes.js
@@ -8,11 +8,11 @@
 // @include https://*/*
 // @name BetterPonymotes
 // @namespace http://rainbow.mlas1.us/
-// @require bpm-data.js?p=2&dver=86
-// @require pref-setup.js?p=2&cver=55
+// @require bpm-data.js?p=2&dver=87
+// @require pref-setup.js?p=2&cver=56
 // @run-at document-start
 // @updateURL http://rainbow.mlas1.us/betterponymotes.user.js
-// @version 55.86
+// @version 56.87
 // ==/UserScript==
 
 /*******************************************************************************
@@ -37,8 +37,8 @@ var emote_map, sr_name2id, sr_id2name, tag_name2id, tag_id2name, bpm_backendsupp
 
 var BPM_DEV_MODE = false;
 
-var BPM_CODE_VERSION = "55";
-var BPM_DATA_VERSION = "86";
+var BPM_CODE_VERSION = "56";
+var BPM_DATA_VERSION = "87";
 var BPM_RESOURCE_PREFIX = "http://rainbow.mlas1.us";
 var BPM_OPTIONS_PAGE = BPM_RESOURCE_PREFIX + "/options.html";
 
