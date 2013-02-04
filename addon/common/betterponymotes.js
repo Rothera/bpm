968696fdd424b7c51320b37ac91c9954d3cdcae1
Rothera <rothera@users.noreply.github.com>
2013-02-04 16:36:02 -0500

    Release v57.90.3 to fix the Opera build

--- addon/common/betterponymotes.js
+++ addon/common/betterponymotes.js
@@ -8,11 +8,11 @@
 // @include https://*/*
 // @name BetterPonymotes
 // @namespace http://rainbow.mlas1.us/
-// @require bpm-data.js?p=2&dver=90.2
+// @require bpm-data.js?p=2&dver=90.3
 // @require pref-setup.js?p=2&cver=57
 // @run-at document-start
 // @updateURL http://rainbow.mlas1.us/betterponymotes.user.js
-// @version 57.90.2
+// @version 57.90.3
 // ==/UserScript==
 
 /*******************************************************************************
@@ -38,7 +38,7 @@ var emote_map, sr_name2id, sr_id2name, tag_name2id, tag_id2name, bpm_backendsupp
 var BPM_DEV_MODE = false;
 
 var BPM_CODE_VERSION = "57";
-var BPM_DATA_VERSION = "90.2";
+var BPM_DATA_VERSION = "90.3";
 var BPM_RESOURCE_PREFIX = "http://rainbow.mlas1.us";
 var BPM_OPTIONS_PAGE = BPM_RESOURCE_PREFIX + "/options.html";
 
