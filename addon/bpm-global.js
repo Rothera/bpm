e8f7d05de62cd0e2dd22c92cc27906e236f45439
Rothera <rothera@users.noreply.github.com>
2013-10-06 20:56:41 -0400

    BGM Chrome fix

--- addon/bpm-global.js
+++ addon/bpm-global.js
@@ -210,7 +210,7 @@ function global_main(store) {
     // Check against domain blacklist
     for(var i = 0; i < DOMAIN_BLACKLIST.length; i++) {
         if(document.location.host === DOMAIN_BLACKLIST[i] ||
-           document.location.host.endsWith(DOMAIN_BLACKLIST[i])) {
+           ends_with(document.location.host, DOMAIN_BLACKLIST[i])) {
             log_warning("Refusing to run on '" + document.location.host + "': domain is blacklisted (probably broken)");
             return;
         }
